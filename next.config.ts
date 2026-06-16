7468e06289fdece10d226181a39e543e2c07cc6d
Pixel Avatar <b62885573@gmail.com>
2026-06-17 03:19:38 +0700

    Fix Vercel sharp optional dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sharp"],
   outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/.pnpm/@img+sharp*/**/*",
+      "./node_modules/.pnpm/sharp*/**/*",
+      "./node_modules/@img/**/*",
+      "./node_modules/sharp/**/*",
+    ],
     "/api/pixelate": [
       "./node_modules/sharp/**/*",
       "./node_modules/@img/**/*",
