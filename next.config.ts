7371040490572b99b180db3039346d067990fafc
Pixel Avatar <b62885573@gmail.com>
2026-06-17 03:15:49 +0700

    Include sharp native files in Vercel function

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sharp"],
+  outputFileTracingIncludes: {
+    "/api/pixelate": [
+      "./node_modules/sharp/**/*",
+      "./node_modules/@img/**/*",
+      "./node_modules/.pnpm/sharp*/**/*",
+      "./node_modules/.pnpm/@img+sharp*/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
